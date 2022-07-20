6fb48eea1e6b9b94610a6d516a4057e286f100fd
xttt <georgeluo@sjtu.edu.cn>
2022-07-21 01:00:32 +0800

    bcrypt

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption")
+const bcrypt = require("bcrypt")
+const saltRound = 10;
 
 const app = express();
 mongoose.connect("mongodb://localhost:27017/userDB")
@@ -15,8 +17,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
-userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -39,16 +39,18 @@ app.get("/login", function(req,res) {
 })
 
 app.post("/register", function(req,res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  })
-  newUser.save(function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets")
-    }
+  bcrypt.hash(req.body.password, saltRound, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    })
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets")
+      }
+    })
   })
 });
 
@@ -61,9 +63,11 @@ app.post("/login", function(req,res) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result) {
+            res.render("secrets");
+          }
+        })
       }
     }
   })
